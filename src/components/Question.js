4e7a9fa2584c768932e88092f9f5e13f9816f615
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2022-07-06 15:58:20 +0100

    spelling-drill

--- src/components/Question.js
+++ src/components/Question.js
@@ -20,30 +20,42 @@ const Question = ({ data, onAnswerUpdate, numberOfQuestions, activeQuestion, onS
   }
   
   const nextClickHandler = (e) => {
+  
     if(selected === '') {
-      return setError('Please select one option!');
+      return setError('Type Something');
     }
     onAnswerUpdate(prevState => [...prevState, { q: data.question, a: selected }]);
     setSelected('');
     if(activeQuestion < numberOfQuestions - 1) {
       onSetActiveQuestion(activeQuestion + 1);
+
     }else {
       onSetStep(3);
+      
     }
+
+  
+
+    
   }
 
   return(
     <div className="card">
       <div className="card-content">
         <div className="content">
+        <p>Question {data.id}</p>
           <h2 className="mb-5">{data.question}</h2>
+          <audio src={data.audio} controls />
           <div className="control" ref={radiosWrapper}>
-            {data.choices.map((choice, i) => (
+          <input type='text' className="input is-success" onChange={changeHandler} name="answer" placeholder="enter answer"/>
+
+            {/* {data.choices.map((choice, i) => (
               <label className="radio has-background-light" key={i}>
                 <input type="radio" name="answer" value={choice} onChange={changeHandler} />
+                
                 {choice}
               </label>
-            ))}
+            ))} */}
           </div>
           {error && <div className="has-text-danger">{error}</div>}
           <button className="button is-link is-medium is-fullwidth mt-4" onClick={nextClickHandler}>Next</button>
